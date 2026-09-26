ce50acdfe140ac4289f5b4f06861f633bb3559aa
sapnucharlotte-spec <sapnucharlotte@gmail.com>
2026-09-26 15:18:32 +0800

    midterm

---
+++ midterm/petAd.py
@@ -0,0 +1,29 @@
+"""
+Midterm Practical Exam — Pet Adoption Records Manager
+Student: Sapnu, Charlotte G.
+"""
+
+pets_list = ["Dog", "Cat", "Bird", "Hamster"]
+
+def display_menu():
+    for item in pets_list:
+        print(item)
+
+
+def add_pet(pets_list):
+    for item in pets_list:
+        petn = input ("Name of the Pet")
+
+def view_pets():
+    pass
+
+def count_available_adopted(pets_list):
+    for item in pets_list:
+         if item == ""
+
+def find_pet(pets_list):
+   
+    pass
+
+if __name__ == "__main__":
+    main()
