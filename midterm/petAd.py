bd0363b5d41ae62cf95031626df35d784a2915dc
sapnucharlotte-spec <sapnucharlotte@gmail.com>
2026-09-26 15:32:31 +0800

    update1

--- midterm/petAd.py
+++ midterm/petAd.py
@@ -3,26 +3,34 @@ Midterm Practical Exam — Pet Adoption Records Manager
 Student: Sapnu, Charlotte G.
 """
 
-pets_list = ["Dog", "Cat", "Bird", "Hamster"]
+pets_list = []
 
 def display_menu():
     for item in pets_list:
         print(item)
 
-petn = input ("Add new Name of the Pet:")
-petc = input ("Type the classification of the pet:")
+def add_pet(name, type, status):
+    while True:
+        name = input("Name of the Pet:")
+        type = input("Type of the Pe:")
+        status = input("Type the Status of the Pet:")
 
-def add_pet(pets_list):
-    for item in pets_list:
-        pass
-
-
+        if name and type and status != "":
+            pets_list.append({"name": name, "type": type, "status" : status})
+            print("Added to the lists!")
+        elif name or type or status == "":
+            pets_list.append({"name": name, "type": type, "status" : status})
+            print("There's missing to fill in.")
+        elif name or type or status == 0:
+                    pets_list.append({"name": name, "type": type, "status" : status})
+                    print("No numbers.")
+        
 def view_pets():
     pass
 
 def count_available_adopted(pets_list):
     for item in pets_list:
-         if item == 
+         if item ==
          pass
 
 def find_pet(pets_list):
