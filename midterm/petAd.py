cfea45798714ea33a306be0de8310a6c3c9eb287
sapnucharlotte-spec <sapnucharlotte@gmail.com>
2026-09-26 15:20:49 +0800

    update

--- midterm/petAd.py
+++ midterm/petAd.py
@@ -9,17 +9,21 @@ def display_menu():
     for item in pets_list:
         print(item)
 
+petn = input ("Add new Name of the Pet:")
+petc = input ("Type the classification of the pet:")
 
 def add_pet(pets_list):
     for item in pets_list:
-        petn = input ("Name of the Pet")
+        pass
+
 
 def view_pets():
     pass
 
 def count_available_adopted(pets_list):
     for item in pets_list:
-         if item == ""
+         if item == 
+         pass
 
 def find_pet(pets_list):
    
